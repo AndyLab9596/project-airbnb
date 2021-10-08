716978a235e4f782f0ea880b0bcdb455047d95e5
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-08 15:00:38 +0700

    header basis html and csss

--- client-side/src/layouts/MainLayout/Header/index.jsx
+++ client-side/src/layouts/MainLayout/Header/index.jsx
@@ -1,10 +1,55 @@
+import { AppBar, Box, Chip, IconButton, Toolbar } from '@material-ui/core';
+import AccountCircleOutlinedIcon from '@material-ui/icons/AccountCircleOutlined';
+import LanguageIcon from '@material-ui/icons/Language';
+import MenuOutlinedIcon from '@material-ui/icons/MenuOutlined';
 import React from 'react';
+import airbnbIcon from '../../../assets/img/airbnblogo.png';
+import useStyles from './style';
 
 const Header = () => {
+    const classes = useStyles()
     return (
-        <div>
-            <h3>Header</h3>
-        </div>
+        <AppBar position="sticky" className={classes.root}>
+            <Toolbar className={classes.navbar__content}>
+                <Box>
+                    <a href="/" target="_blank" rel="noreferrer">
+                        <img src={airbnbIcon} alt="icon" className={classes.navbar__content__icon} />
+                    </a>
+                </Box>
+
+                <Box className={classes.navbar__content__menu}>
+                    <span>
+                        Nơi ở
+                    </span>
+                    <span>
+                        Trải nghiệm
+                    </span>
+                    <span>
+                        Trải nghiệm trực tuyến
+
+                    </span>
+                </Box>
+
+                <div className={classes.navbar__content__left}>
+                    <span >
+                        Trở thành chủ nhà
+                    </span>
+                    <IconButton className={classes.language__icon}>
+                        <LanguageIcon fontSize="small" />
+                    </IconButton>
+                    <Chip
+                        className={classes.chip}
+                        size="medium"
+                        icon={<MenuOutlinedIcon fontSize="small" />}
+                        label={<AccountCircleOutlinedIcon fontSize="medium" />}
+                        // onClick={handleClick}
+                        // onDelete={handleDelete}
+                        variant="default"
+                        color="#ffffff"
+                    />
+                </div>
+            </Toolbar>
+        </AppBar>
     );
 };
 
---
+++ client-side/src/layouts/MainLayout/Header/style.js
@@ -0,0 +1,54 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+export default makeStyles(theme => ({
+    root: {
+        height: '80px',
+        width: '100%',
+        margin: 'auto',
+        padding: '0 40px',
+        paddingBottom: 0,
+        marginBottom: 0,
+
+    },
+    navbar__content: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        height: '100%',
+    },
+    navbar__content__icon: {
+        height: '35px',
+    },
+    navbar__content__menu: {
+        display: 'flex',
+        flex: 1,
+        maxWidth: 'fit-content',
+        alignItems: 'center',
+        '& > span': {
+            fontSize: '1rem',
+            lineHeight: '20px',
+            fontWeight: 400,
+            padding: '10px 16px'
+        }
+    },
+
+    navbar__content__left: {
+        display: 'flex',
+        alignItems: 'center',
+        '& > span': {
+            marginRight: '8px'
+        }
+    },
+
+    language__icon: {
+        color: '#fff',
+
+    },
+
+    chip: {
+        height: '42px',
+        borderRadius: '21px'
+    }
+
+
+}))
