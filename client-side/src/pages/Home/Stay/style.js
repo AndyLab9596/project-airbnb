242535cc2584afe2cf0f18c5da876bac2c9a8e3b
Phát Lã <lavantuanphat15@gmail.com>
2021-10-09 18:11:41 +0700

    update to stayV2

--- client-side/src/pages/Home/Stay/style.js
+++ client-side/src/pages/Home/Stay/style.js
@@ -9,7 +9,23 @@ export default makeStyles(theme => ({
         lineHeight: "52px",
         letterSpacing: 0,
         paddingBottom: 16
+    },
+    stay__item: {
+        padding: "7px",
+        cursor: "pointer",
+
+    },
+    stay__img: {
+        objectFit: "cover",
+        borderRadius: "8px",
+        width: "100%",
+        height: "100%",
+        // padding: "7px",
+        // margin:7
+    },
+    stay__content: {
+        fontWeight: 600,
+        fontSize: "18px",
+        lineHeight: "22px"
     }
-
-
 }))
