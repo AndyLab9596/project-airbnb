8aa30aa86d47a2c886d833708434e0ec64de875a
Huy Quang <buidinhquanghuy0312@gmail.com>
2021-10-09 16:51:47 +0700

    update theme

--- client-side/src/constants/config.js
+++ client-side/src/constants/config.js
@@ -4,4 +4,17 @@ export const BASE_URL = "https://airbnb.cybersoft.edu.vn/api/";
 export const TOKEN_BY_CLASS =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCAwOSIsIkhldEhhblN0cmluZyI6IjI3LzAxLzIwMjIiLCJIZXRIYW5UaW1lIjoiMTY0MzI0MTYwMDAwMCIsIm5iZiI6MTYxNjM0NjAwMCwiZXhwIjoxNjQzMzg5MjAwfQ.NEQRF8SKORq7R7kYbYCCO9ZZXYxTWlbaTc2wxXWMfiw";
 
-export const theme = createTheme();
+export const theme = createTheme({
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 375,
+      md: 744,
+      lg: 950,
+      xl: 1128,
+      xxl: 1440,
+      home: 1760,
+      detail: 1280,
+    },
+  },
+});
