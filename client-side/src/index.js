7e4390ef99559f2f8ef662322afcdde2f293d612
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-09 18:56:56 +0700

    update

--- client-side/src/index.js
+++ client-side/src/index.js
@@ -4,13 +4,10 @@ import "./index.css";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./store";
-<<<<<<< HEAD
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-=======
 import "../src/constants/config.css";
 
->>>>>>> master
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
