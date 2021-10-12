08d0789339601556504ec40e2d8d1d84cf4ff53b
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-12 21:40:29 +0700

    update home banner

--- client-side/src/pages/Home/HomeBanner/style.js
+++ client-side/src/pages/Home/HomeBanner/style.js
@@ -4,7 +4,8 @@
 import { makeStyles } from '@material-ui/core/styles';
 export default makeStyles(theme => ({
     banner: {
-        backgroundImage: props => `url(${props.bannerImg})`,
+        // backgroundImage: props => `url(${props.bgImgW774})`,
+        backgroundImage: props => props.isTablet ? `url(${props.bgImgW960})` : `url(${props.bgImgW774})`,
         backgroundRepeat: "no-repeat",
         backgroundSize: "cover",
         backgroundPosition: "center center",
