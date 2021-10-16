760a22a0fe3cca1c1506f490f235fac924a56f27
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-16 08:40:37 +0700

    transform iife into useSelector

--- client-side/src/pages/Home/ExploreNearby/index.jsx
+++ client-side/src/pages/Home/ExploreNearby/index.jsx
@@ -1,35 +1,37 @@
-import React, { useEffect, useState } from "react";
 import { Container, Grid, Typography } from "@material-ui/core";
-import manageLocationApi from '../../../api/manageLocationApi';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { getLocations } from "../../../store/action/LocationAction";
 import useStyles from "./style";
-import { useHistory } from "react-router-dom"
-import { useDispatch } from "react-redux";
-import { GET_LOCATIONS } from "../../../store/types/LocationType";
 
 const ExploreNearby = () => {
 
 
   const history = useHistory();
   const dispatch = useDispatch();
-  const [cities, setCities] = useState([]);
+  const { locations } = useSelector(state => state.LocationReducer);
+  console.log(locations)
 
   const handleCityClick = (locationId) => {
     history.push(`/list/${locationId}`)
   }
 
+
   useEffect(() => {
-    (async () => {
-      try {
-        const response = await manageLocationApi.getAll()
-        setCities(response);
-        dispatch(GET_LOCATIONS, response)
+    // (async () => {
+    //   try {
+    //     const response = await manageLocationApi.getAll()
+    //     setCities(response);
+    //     dispatch(GET_LOCATIONS, response)
 
-      } catch (error) {
-        console.log(error)
-      }
-    })()
+    //   } catch (error) {
+    //     console.log(error)
+    //   }
+    // })()
+    dispatch(getLocations())
 
-  }, [])
+  }, [dispatch])
 
 
   const classes = useStyles();
@@ -42,7 +44,7 @@ const ExploreNearby = () => {
         </Typography>
 
         <Grid container spacing={2}>
-          {cities.map((city, index) => (
+          {locations?.map((city, index) => (
             <Grid item xs={12} sm={6} md={3} key={index}>
               <div className={classes.cities} onClick={() => handleCityClick(city._id)}>
                 <img src={city.image} alt="city" className={classes.city__img} />
