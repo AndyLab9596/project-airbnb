bdf2a67d9ea44a08f88223261870a7cb38210c8b
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-23 15:06:09 +0700

    finish ui grid card

--- client-side/src/pages/ListRoomVer2/MobileListRoom/index.jsx
+++ client-side/src/pages/ListRoomVer2/MobileListRoom/index.jsx
@@ -1,9 +1,42 @@
+import { Grid } from '@material-ui/core';
+import StarIcon from '@material-ui/icons/Star';
+import queryString from 'query-string';
 import React, { useEffect, useState } from 'react';
 import { useLocation, useParams } from 'react-router';
+import { formMoney } from '../../../utilities/coordinates';
 import Mapbox from '../Mapbox';
-import useStyles from "./style";
-import queryString from 'query-string';
 import useFetch from '../useFetch';
+import useStyles from "./style";
+
+
+
+const fakeRoom = {
+    "_id": "61699651efe193001c0a5bda",
+    "name": "KHA Hostel & Cafe",
+    "guests": 1,
+    "bedRoom": 1,
+    "bath": 2,
+    "description": "Kiến trúc độc đáo",
+    "price": 200000,
+    "elevator": true,
+    "hotTub": true,
+    "pool": true,
+    "indoorFireplace": true,
+    "dryer": true,
+    "gym": true,
+    "kitchen": false,
+    "wifi": true,
+    "heating": true,
+    "cableTV": true,
+    "image": "https://airbnb.cybersoft.edu.vn/public/images/room/1634310184889_khahostel.jpg",
+    "locationId": {
+        "name": "Hồ Chí Minh",
+        "province": "Hồ Chí Minh",
+        "country": "VN",
+        "valueate": 8,
+        "image": "https://airbnb.cybersoft.edu.vn/public/temp/1634304216447_saigonbuivien.jpg"
+    }
+};
 
 const MobileListRoom = () => {
 
@@ -18,6 +51,10 @@ const MobileListRoom = () => {
     const [rentRooms, setRentRooms] = useState([]);
 
     const [page, setPage] = useState(1);
+
+    const handlePageChange = (event, page) => {
+        setPage(page)
+    }
 
 
     useEffect(() => {
@@ -49,6 +86,7 @@ const MobileListRoom = () => {
             {/* Content */}
             <div className={classes.content}>
                 <div className={classes.wrapper}>
+                    {/* Header */}
                     <div className={classes.header}>
                         <div className={classes.header__content}>
                             <h6 className={classes.header__content__text}>
@@ -56,6 +94,47 @@ const MobileListRoom = () => {
                             </h6>
                         </div>
                     </div>
+
+                    {/* Cards */}
+                    <section className={classes.section}>
+                        <Grid container spacing={3}>
+                            <Grid item xs={12}>
+                                <div className={classes.card}>
+                                    <div className={classes.card__media}>
+                                        <img src={fakeRoom.image} alt={fakeRoom.name}
+                                            className={classes.card__media__img} />
+                                    </div>
+                                    <div className={classes.card__content}>
+
+                                        <div className={classes.card__content_evaluate}>
+                                            <span>
+                                                <StarIcon className={classes.evaluate__icon} />
+                                            </span>
+                                            <span className={classes.evaluate__points}>
+                                                {fakeRoom.locationId.valueate}
+                                            </span>
+                                            <span className={classes.evaluate__number}>
+                                                ( 56 đánh giá )
+                                            </span>
+                                        </div>
+
+                                        <div className={classes.card__content__name}>
+                                            <h3>
+                                                {fakeRoom.name}
+                                            </h3>
+                                        </div>
+
+                                        <div className={classes.card__content__price}>
+                                            <p>
+                                                {formMoney(fakeRoom.price)} / <span>đêm</span>
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Grid>
+                        </Grid>
+                    </section>
+
                 </div>
             </div>
 
--- client-side/src/pages/ListRoomVer2/MobileListRoom/style.js
+++ client-side/src/pages/ListRoomVer2/MobileListRoom/style.js
@@ -43,12 +43,16 @@ export default makeStyles(theme => ({
         // borderTopLeftRadius: '24px',
         // borderTopRightRadius: '24px',
         minHeight: 'calc(60vh + 65px)',
+
     },
 
     wrapper: {
         minHeight: '30xp',
         width: '100%',
         position: 'unset',
+        maxWidth: '640xp',
+        marginLeft: 0,
+        marginRight: 0,
         // borderTopLeftRadius: '24px',
         // borderTopRightRadius: '24px',
     },
@@ -85,13 +89,87 @@ export default makeStyles(theme => ({
         marginRight: '24px',
         minHeight: '64px',
         textAlign: 'center',
+
     },
 
     header__content__text: {
         fontSize: '16px',
-        margin: 0,
-        padding: 0,
+
     },
+
+    section: {
+        width: '85vw',
+        margin: '8px auto',
+    },
+
+    card: {
+        position: 'relative',
+        marginTop: '8px',
+
+    },
+    card__media: {
+        position: 'relative',
+
+    },
+
+    card__media__img: {
+        display: 'block',
+        objectFit: 'cover',
+        width: '100%',
+        borderRadius: '12px',
+    },
+
+    card__content: {
+        marginTop: 10,
+
+    },
+
+    card__content_evaluate: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+        fontSize: '14px',
+        lineHeight: '18px',
+        marginBottom: '6px',
+        '& > span': {
+            marginRight: '4px'
+        }
+    },
+
+    card__content__name: {
+        '& > h3': {
+            margin: '0',
+            fontSize: '18px',
+            lineHeight: '24px',
+            color: '#222222',
+            fontWeight: 400
+        },
+
+    },
+
+    card__content__price: {
+        marginTop: '4px',
+        fontSize: '18px',
+        lineHeight: '24px',
+        fontWeight: 600,
+        color: '#222222',
+        '& > p': {
+            margin: 0,
+        },
+        '& >span': {
+            color: '#717171'
+        }
+
+    }
+
+
+
+
+
+
+
+
+
 
 
 
