934e990dc49a14300e6ab2c2a5117c94420fc748
Phát Lã <lavantuanphat15@gmail.com>
2021-10-28 15:33:02 +0700

    update app

--- client-side/src/App.js
+++ client-side/src/App.js
@@ -102,7 +102,7 @@ const App = () => {
             Component={AddRating}
           />
 
-          {/* <AdminLayout path="/admin" Component={Admin} /> */}
+          <AdminLayout path="/admin" Component={Admin} />
         </Switch>
       </ThemeProvider>
     </BrowserRouter>
