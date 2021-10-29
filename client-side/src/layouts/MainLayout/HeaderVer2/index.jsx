91465fa100ca24a5d67a78f9e557dc2a2f3bb1a0
AndyLab9596 <79738559+AndyLab9596@users.noreply.github.com>
2021-10-29 10:15:48 +0700

    update header

--- client-side/src/layouts/MainLayout/HeaderVer2/index.jsx
+++ client-side/src/layouts/MainLayout/HeaderVer2/index.jsx
@@ -153,8 +153,8 @@ const HeaderVer2 = () => {
                         classes.appBar,
                         { [classes.appBarFixed]: homePagePath, },
                         { [classes.appBarFixedList]: listPagePath },
-                        { [classes.appBarStaticDetail]: detailPagePath || profilePagePath },
-                        { [classes.appBarStaticPay]: payPagePath }
+                        { [classes.appBarStaticDetail]: detailPagePath },
+                        { [classes.appBarStaticPay]: payPagePath || profilePagePath }
                     )}
                 >
                     <div className={classes.wrapper}>
@@ -465,7 +465,7 @@ const HeaderVer2 = () => {
                     <div className={clsx(
                         { [classes.searchBar]: homePagePath },
                         { [classes.list__searchBar]: listPagePath || detailPagePath },
-                        { [classes.pay__searchBar]: payPagePath },
+                        { [classes.pay__searchBar]: payPagePath || profilePagePath },
                         {
                             [classes.hideSearchBar]: !displaySearchBar,
                         }
